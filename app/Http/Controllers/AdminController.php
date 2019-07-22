32c36ddf145472a303213ebd55aefe9f86b40ea9
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-07-22 15:46:47 +0545

    Commit from Protect Admin Routes

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Session;
+
+class AdminController extends Controller
+{
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->input();
+            if(Auth::attempt(['email' => $data['email'], 'password' => $data['password'], 'admin' => 1])){
+               return redirect()->route('admin.dashboard');
+            } else {
+                return redirect('/admin')->with('flash_message_error', 'Invalid Username or Password');
+            }
+        }
+        return view ('admin.admin_login');
+    }
+
+    public function dashboard(){
+        return view ('admin.dashboard');
+    }
+
+    public function logout(){
+        Session::flush();
+        return redirect('/admin')->with('flash_message_success', 'Logout Successfull');
+    }
+}
