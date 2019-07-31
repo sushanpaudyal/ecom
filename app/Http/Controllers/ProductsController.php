b35ac4a3f3619619f2686bfc8b1e18ae77fbd46e
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-07-31 12:52:09 +0545

    Delete Products Images from folders

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -131,6 +131,24 @@ class ProductsController extends Controller
     }
 
     public function deleteProductImage($id = null){
+        $productImage = Product::where(['id' => $id])->first();
+        // Get Image Path
+        $large_image_path = 'images/backend_images/products/large/';
+        $medium_image_path = 'images/backend_images/products/medium/';
+        $small_image_path = 'images/backend_images/products/small/';
+
+        if(file_exists($large_image_path.$productImage->image)){
+            unlink($large_image_path.$productImage->image);
+        }
+
+        if(file_exists($medium_image_path.$productImage->image)){
+            unlink($medium_image_path.$productImage->image);
+        }
+
+        if(file_exists($small_image_path.$productImage->image)){
+            unlink($small_image_path.$productImage->image);
+        }
+
         Product::where(['id' => $id])->update(['image' => '']);
         return redirect()->back()->with('flash_message_success', 'Product Image Deleted Successfully');
     }
