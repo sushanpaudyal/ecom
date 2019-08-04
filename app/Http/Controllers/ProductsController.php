60e5a6867dd78b42aba8115967b8c741e194eadc
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-08-04 17:40:58 +0545

    Prevent Duplicating SKU for product

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -175,6 +175,17 @@ class ProductsController extends Controller
             $data = $request->all();
             foreach($data['sku'] as $key => $val){
                 if(!empty($val)){
+                    // SKU Check
+                    $attrCountSKU = ProductsAttribute::where('sku', $val)->count();
+                    if($attrCountSKU > 0){
+                        return redirect()->back()->with('flash_message_error', 'SKU Already Exits');
+                    }
+
+                    // Size Check
+                    $attrCountSizes = ProductsAttribute::where(['product_id' => $id, 'size' => $data['size'][$key]])->count();
+                    if($attrCountSizes > 0){
+                        return redirect()->back()->with('flash_message_error', 'Size Already Exits');
+                    }
                     $attribute = new ProductsAttribute;
                     $attribute->product_id = $id;
                     $attribute->sku = $val;
