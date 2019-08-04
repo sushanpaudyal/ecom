ddacaee603ee1f6350cb9f3d4f7bbae2442b6053
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-08-04 23:08:14 +0545

    Easy Zoom

--- resources/views/layouts/frontLayout/front_design.blade.php
+++ resources/views/layouts/frontLayout/front_design.blade.php
@@ -13,6 +13,7 @@
     <link href="{{asset('css/frontend_css/price-range.css')}}" rel="stylesheet">
     <link href="{{asset('css/frontend_css/animate.css')}}" rel="stylesheet">
     <link href="{{asset('css/frontend_css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('css/frontend_css/easyzoom.css')}}" rel="stylesheet">
     <link href="{{asset('css/frontend_css/responsive.css')}}" rel="stylesheet">
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
@@ -37,6 +38,8 @@
 <script src="{{asset('js/frontend_js/jquery.scrollUp.min.js')}}"></script>
 <script src="{{asset('js/frontend_js/price-range.js')}}"></script>
 <script src="{{asset('js/frontend_js/jquery.prettyPhoto.js')}}"></script>
+<script src="{{asset('js/frontend_js/easyzoom.js')}}"></script>
 <script src="{{asset('js/frontend_js/main.js')}}"></script>
+
 </body>
 </html>
