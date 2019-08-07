b2be1b2167629530e0ce6b765bc79536d10d7ef3
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-08-07 22:38:38 +0545

    jquery datepicker

--- resources/views/layouts/adminLayout/admin_design.blade.php
+++ resources/views/layouts/adminLayout/admin_design.blade.php
@@ -13,8 +13,10 @@
     <link rel="stylesheet" href="{{asset('css/backend_css/matrix-media.css')}}" />
     <link href="{{asset('fonts/backend_fonts/css/font-awesome.css')}}" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('css/backend_css/jquery.gritter.css')}}" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
 </head>
 <body>
 
@@ -24,28 +26,37 @@
 
 @yield('content')
 
-<!--Footer-part-->
+
 
 @include('layouts.adminLayout.admin_footer')
 
 
-<!--end-Footer-part-->
 
 
 
 
 <script src="{{asset('js/backend_js/jquery.min.js')}}"></script>
-<script src="{{asset('js/backend_js/jquery.ui.custom.js')}}"></script>
 <script src="{{asset('js/backend_js/bootstrap.min.js')}}"></script>
 <script src="{{asset('js/backend_js/jquery.uniform.js')}}"></script>
-<script src="{{asset('js/backend_js/select2.min.js')}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js">
+</script>
+<script src="{{asset('js/backend_js/select2.min.js')}}"></script>
 <script src="{{asset('js/backend_js/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('js/backend_js/jquery.validate.js')}}"></script>
 <script src="{{asset('js/backend_js/matrix.js')}}"></script>
 <script src="{{asset('js/backend_js/matrix.form_validation.js')}}"></script>
 <script src="{{asset('js/backend_js/matrix.tables.js')}}"></script>
 <script src="{{asset('js/backend_js/matrix.popover.js')}}"></script>
+<script>
+    $( function() {
+        $( "#expiry_date" ).datepicker({
+            minDate: 0,
+            dateFormat: 'yy-mm-dd'
+        });
+    } );
+</script>
+
 </body>
 </html>
