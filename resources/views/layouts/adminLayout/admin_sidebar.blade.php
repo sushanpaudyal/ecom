9cf82f9de06752c464b1048f3d8850d1ebb518cc
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-08-13 22:57:29 +0545

    update sidebar active modules

--- resources/views/layouts/adminLayout/admin_sidebar.blade.php
+++ resources/views/layouts/adminLayout/admin_sidebar.blade.php
@@ -1,32 +1,43 @@
+<?php
+   $url = url()->current();
+?>
+
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
     <ul>
-        <li class="active"><a href="{{route('admin.dashboard')}}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+        <li
+        <?php if(preg_match("/dashboard/i", $url)) { echo 'class="active"'; } ?>    >
+            <a href="{{url('admin/dashboard')}}"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+
+
 
         <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Categories</span> </a>
-            <ul>
-                <li><a href="{{route('addCategory')}}">Add Category</a></li>
-                <li><a href="{{route('viewCategories')}}">View Category</a></li>
+            <ul <?php if(preg_match("/categor/i", $url)) { echo 'style="display:block;"';} ?>>
+                <li <?php if(preg_match("/add-category/i", $url)) { echo 'class="active"';} ?>><a href="{{route('addCategory')}}">Add Category</a></li>
+                <li <?php if(preg_match("/view-categories/i", $url)) { echo 'class="active"';} ?>><a href="{{route('viewCategories')}}">View Category</a></li>
             </ul>
         </li>
+
 
         <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Products</span> </a>
-            <ul>
-                <li><a href="{{route('addProduct')}}">Add Product</a></li>
-                <li><a href="{{route('viewProducts')}}">View Products</a></li>
+            <ul <?php if(preg_match("/product/i", $url)) { echo 'style="display:block;"';} ?>>
+                <li <?php if(preg_match("/add-product/i", $url)) { echo 'class="active"';} ?>><a href="{{route('addProduct')}}">Add Product</a></li>
+                <li <?php if(preg_match("/view-products/i", $url)) { echo 'class="active"';} ?>><a href="{{route('viewProducts')}}">View Product</a></li>
             </ul>
         </li>
+
 
         <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Coupons</span> </a>
-            <ul>
-                <li><a href="{{route('addCoupon')}}">Add Coupons</a></li>
-                <li><a href="{{route('view.coupon')}}">View Coupons</a></li>
+            <ul <?php if(preg_match("/coupon/i", $url)) { echo 'style="display:block;"';} ?>>
+                <li <?php if(preg_match("/add-coupon/i", $url)) { echo 'class="active"';} ?>><a href="{{route('addCoupon')}}">Add Coupon</a></li>
+                <li <?php if(preg_match("/view-coupons/i", $url)) { echo 'class="active"';} ?>><a href="{{route('view.coupon')}}">View Coupons</a></li>
             </ul>
         </li>
 
+
         <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Banners</span> </a>
-            <ul>
-                <li><a href="{{route('add.banner')}}">Add Banners</a></li>
-                <li><a href="{{route('view.banner')}}">View Banners</a></li>
+            <ul <?php if(preg_match("/banner/i", $url)) { echo 'style="display:block;"';} ?>>
+                <li <?php if(preg_match("/add-banner/i", $url)) { echo 'class="active"';} ?>><a href="{{route('add.banner')}}">Add Banner</a></li>
+                <li <?php if(preg_match("/view-banners/i", $url)) { echo 'class="active"';} ?>><a href="{{route('view.banner')}}">View Banner</a></li>
             </ul>
         </li>
 
